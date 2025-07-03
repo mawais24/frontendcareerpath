e17b8151d56f54dabcaf7790e0ccca3285fd5f79
Muhammad Awais <mr.mawais24@gmail.com>
2025-07-03 23:12:47 +0500

    all classes data

---
+++ class16/js/main.js
@@ -0,0 +1,22 @@
+// Create Two Variables, myAge and humanDogRatio
+// Mulitple both variables and then store in myDogAge
+// myDogAge console display
+
+let myAge = 34;
+let humanDogRatio = 7;
+
+let myDogAge = myAge * humanDogRatio;
+
+console.log(myDogAge);
+
+//Variable Declaration
+//Variable Initiliazation
+
+// let x = 10;
+// let y = 25;
+
+// let z = x * y;
+
+// console.log(z);
+
+// document.getElementById("paragraph").innerText = z;
