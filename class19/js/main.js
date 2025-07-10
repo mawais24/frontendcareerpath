ea692a099e9c6684d7ba7d137804283b81a7ee48
Muhammad Awais <mr.mawais24@gmail.com>
2025-07-10 23:34:28 +0500

    class 17, 18, 19

---
+++ class19/js/main.js
@@ -0,0 +1,25 @@
+document.getElementById("para").innerText = "Hello! i am from js file";
+
+let a = 10;
+let b = 20;
+
+let c = a + b; // 30
+
+c++; // 30 + 1 = 31
+console.log(c);
+c--;
+console.log(c); // 31 - 1 = 30
+
+let count = 0;
+
+function increment() {
+  count = count + 1;
+  console.log(count);
+  document.getElementById("myValue").innerText = count;
+}
+
+// function increment() {
+//   let a = 0;
+//   a++;
+//   document.getElementById("myValue").innerHTML = a;
+// }
